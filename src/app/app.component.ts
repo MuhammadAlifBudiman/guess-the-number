d36d557ba811251332bdcebd01576563276b485d
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-04-04 11:54:02 +0700

    create the game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GuessTheNumberComponent } from './guess-the-number/guess-the-number.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GuessTheNumberComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
